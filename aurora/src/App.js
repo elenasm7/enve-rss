9347ed057ace8bc250392d8f8b3dc5eb161755ba
Komran Ghahremani <gkomran@gmail.com>
2017-11-14 00:50:25 -0500

    counter state example

--- aurora/src/App.js
+++ aurora/src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "count": 0
+    };
+  }
+
+  increment = () => {
+      this.setState({ "count": this.state.count + 1 });
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +25,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.increment}>{this.state.count}</button>
       </div>
     );
   }
