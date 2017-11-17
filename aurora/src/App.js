d036094c3f39d0456420ca34956d684f7ca7454e
Komran Ghahremani <gkomran@gmail.com>
2017-11-17 00:26:10 -0500

    messing with buttons

--- aurora/src/App.js
+++ aurora/src/App.js
@@ -18,14 +18,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.increment}>{this.state.count}</button>
+        <button className="first" onClick={this.increment}>{this.state.count}</button>
+        <button className="second" onClick={this.increment}>{this.state.count}</button>
+        <button className="third" onClick={this.increment}>{this.state.count}</button>
+        <button className="fourth" onClick={this.increment}>{this.state.count}</button>
+        <button className="five" onClick={this.increment}>{this.state.count}</button>
+        <button className="six" onClick={this.increment}>{this.state.count}</button>
       </div>
     );
   }
