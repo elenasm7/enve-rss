0b0ac01ffb019a9662c3ee72ace73335e71bc75d
Komran Ghahremani <gkomran@gmail.com>
2017-12-10 14:31:42 -0500

    cell hover states

--- aurora/src/Story.jsx
+++ aurora/src/Story.jsx
@@ -11,6 +11,7 @@ class Story extends Component {
 			<div className="story-container" onClick={this.updateArticleComponent} >
 				<div className="story-title">{this.props.name}</div>
 				<p className="time">{this.props.subtitle} | {this.props.author}</p>
+				<img className="story-share" alt="share" src="https://image.flaticon.com/icons/svg/97/97930.svg" />
 			</div>
 		);
 	}
