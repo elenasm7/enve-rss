0ea8a1a9197d5749b035ba8d8a92363698e9c831
Elena Morais <elenasm7@Elenas-MacBook-Pro.local>
2017-12-22 00:53:07 -0500

    Added Firebase

--- aurora/src/index.js
+++ aurora/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBpDihzMW3V4I6xnBqAKp25ApzaGegWxJY",
+    authDomain: "aurora-10876.firebaseapp.com",
+    databaseURL: "https://aurora-10876.firebaseio.com",
+    projectId: "aurora-10876",
+    storageBucket: "aurora-10876.appspot.com",
+    messagingSenderId: "974855681516"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
